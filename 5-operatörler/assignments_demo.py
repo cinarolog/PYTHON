527b2e47c4620b89f29230484d3089a6b790c64c
Muhammet_Cinar <mhmmtcnr81@gmail.com>
2022-04-27 02:31:16 +0300

    py
    py

---
+++ 5-operatörler/assignments_demo.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 21:55:01 2022
+
+@author: cinar
+"""
+
+a, b, c = 2, 5, 12
+
+# 1- Kullanıcıdan aldığınız 2 sayının çarpımı ile a,b,c toplamının farkı nedir ?
+
+x = int(input("x: "))
+y = int(input("y: "))
+
+# sonuc = (x * y) - (a+b+c)
+
+# 2- c' nin  b' ye kalansız bölümünü hesaplayınız.
+sonuc = c // b
+
+# 3- (a,b,c) toplamının mod 3' ü nedir ?
+
+sonuc = (a+b+c) % 3
+
+# 4- b' nin a. kuvvetini hesaplayınız.
+sonuc = b ** a
+
+# 5- a, *b, c = sayilar işlemine göre c' nin küpü kaçtır ? 
+sayilar = 1, 5, 7, 10, 3
+a, *b, c = sayilar
+print(c ** 3)
+
+# 6- a, *b, c = sayilar işlemine göre b nin değerleri toplamı kaçtır ?
+a, *b, c = sayilar
+print(b[0] + b[1] + b[2])
+
+
+
