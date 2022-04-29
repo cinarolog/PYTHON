c872c7c158c980e657e92a54655a3ea4b51a0d6d
Muhammet_Cinar <mhmmtcnr81@gmail.com>
2022-04-30 01:51:57 +0300

    py
    py

---
+++ 9-fonksiyonlar/_bankamatik.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 30 00:08:01 2022
+
+@author: cinar
+"""
+
+CinarHesap = {
+    'ad': 'cinarolog',
+    'hesapNo': '13678',
+    'bakiye': 3000,
+    'ekHesap': 2000
+}
+
+AileHesap = {
+    'ad': 'Aile',
+    'hesapNo': '123478',
+    'bakiye': 50000,
+    'ekHesap': 30000
+}
+
+
+def paraCek(hesap, miktar):
+    print(f"Merhaba {hesap['ad']}")
+
+    if (hesap['bakiye'] >= miktar):
+        hesap['bakiye'] -= miktar 
+        print('paranızı alabilirsiniz.')
+        bakiyeSorgula(hesap)
+    else:
+        toplam = hesap['bakiye'] + hesap['ekHesap']
+
+        if (toplam >= miktar):
+            ekHesapKullanimi = input('ek hesap kullanılsın mı (e/h)')
+
+            if ekHesapKullanimi == 'e':
+                ekhesapKullanilacakMiktar = miktar - hesap['bakiye']
+                hesap['bakiye'] = 0
+                hesap['ekHesap'] -= ekhesapKullanilacakMiktar
+                print('paranızı alabilirsiniz.')
+                bakiyeSorgula(hesap)
+            else:
+                print(f"{hesap['hesapNo']} nolu hesabınızda {hesap['bakiye']} bulunmaktadır.")
+        else:
+            print('üzgünüz bakiye yetersiz')
+            bakiyeSorgula(hesap)
+
+
+def bakiyeSorgula(hesap):
+    print(f"{hesap['hesapNo']} nolu hesabınızda {hesap['bakiye']} TL bulunmaktadır. Ek hesap limitiniz ise {hesap['ekHesap']} TL bulunmaktadır.")
+
+paraCek(CinarHesap, 3000)
+
+print('*****************')
+
+paraCek(CinarHesap, 2000)
+
+
+
+
+
+
+
+
+
+
