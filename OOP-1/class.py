8dee75f718635e43f48f92a0d6213a1b6b3a1728
Muhammet_Cinar <mhmmtcnr81@gmail.com>
2022-05-11 12:54:37 +0300

    py
    py

---
+++ OOP-1/class.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 11 12:46:14 2022
+
+@author: cinar
+"""
+
+
+class Student:
+    #method
+    #attrıbute
+    pass
+
+
+    
+
+ogrenci1=Student()
+ogrenci2=Student()
+
+type(ogrenci1)
+
+
+class Product:
+    pass
+
+
+p1=Product()
+p2=Product()
+p3=Product()
+products=[p1,p2,p3]
+
+
+for p in products:
+    print(type(p))
+
+
+
+
